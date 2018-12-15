026e5a4be20e24dd413001682f6a8acc451d01f4
conormccauley1999 <conormccauley1999@gmail.com>
2018-12-15 06:25:07 +0000

    Fixed formatting

--- constants.js
+++ constants.js
@@ -3,8 +3,8 @@ const SCENARIO  = 0;
 const AGREE     = 1;
 
 // Results types
-const BARS		= 0;
-const COMPASS	= 1;
+const BARS  	= 0;
+const COMPASS 	= 1;
 
 // Miscellaneous
 const ANSWERS_PER_MINUTE = 6;
