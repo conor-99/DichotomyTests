33e4524ddec3602ac129c6e0b02421c6f1413f33
conormccauley1999 <conormccauley1999@gmail.com>
2020-03-31 12:24:24 +0100

    Remove constants.php

--- site/api/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('SQL_INSERT_RESULT', 'insert into result (test_id, ip_address, answers, percentages) values (?, ?, ?, ?)');
-define('DT_SQL_SERVER', 'sql230.main-hosting.eu');
-define('DT_SQL_USER', 'u264460363_dichotomytests');
-define('DT_SQL_PASS', '8i/]c4:\CL5d');
-define('DT_SQL_DATABASE', 'u264460363_dichotomytests');
